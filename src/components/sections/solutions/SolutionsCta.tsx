3b13bb39547e1471ad94ebe13b10effcf89c1100
riyazs0506 <mohammedriyazs0506@gmail.com>
2026-07-22 15:45:45 +0530

    updated

--- src/components/sections/solutions/SolutionsCta.tsx
+++ src/components/sections/solutions/SolutionsCta.tsx
@@ -38,7 +38,7 @@ export function SolutionsCta() {
 
               <Button
                 asChild
-                variant="secondary"
+                variant="outline"
                 size="lg"
                 className="border border-white/15 bg-white/10 text-white hover:bg-white/20"
               >
