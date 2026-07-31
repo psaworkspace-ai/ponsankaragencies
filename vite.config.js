72b0fd783ed8a185336eed2c4731f1a7ba3024c6
riyazs0506 <mohammedriyazs0506@gmail.com>
2026-07-31 16:18:57 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "node:url";
-import { dirname, resolve } from "node:path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { fileURLToPath, URL } from "node:url";
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-    ],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            "@": resolve(__dirname, "src"),
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
 });
